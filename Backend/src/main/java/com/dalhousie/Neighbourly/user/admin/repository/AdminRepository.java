ef71a63cb34e28486520d5d23c1d89825b3d5b4c
msshah <ml677231@dal.ca>
2025-02-05 00:58:37 -0400

    chore: Remove files for restructuring of code

--- Backend/src/main/java/com/dalhousie/Neighbourly/user/admin/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dalhousie.Neighbourly.user.admin.repository;
-
-import com.dalhousie.Neighbourly.user.admin.model.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-}
