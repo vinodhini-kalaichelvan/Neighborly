ba4fe9731bcfb2d8f75d87b22f11325aa27c2244
vinodhini-kalaichelvan <vinodhinispec@gmail.com>
2025-02-07 20:44:54 -0400

    resolved merge conflicts

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,34 +7,30 @@ import Register from "./pages/registration";
 import Login from "./pages/login";
 import AdminDashboard from "./pages/admin";
 import "./index.css";
-<<<<<<< HEAD
 import Homepage from "./pages/Homepage";
-=======
 import ForgotPassword from "./pages/forgot_password";
 import ResetPassword from "./pages/reset_password";
->>>>>>> origin/feature/user_authentication
 
 function App() {
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+return(
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
         <Route path="/" element={<Homepage/>} />
-=======
->>>>>>> origin/feature/user_authentication
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/forgot_password" element={<ForgotPassword />} />
         <Route path="/reset_password" element={<ResetPassword />} />
-       
-
       </Routes>
     </Router>
+);
+}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
+);
 
-)}
- export default App;
+export default App;
 //  <Route path="/about" element={<About />} />
