f200f532ab6484a2453e58759351ba1ed44b7b36
Prosper Collins <pr782734@dal.ca>
2025-02-14 12:08:47 -0400

    Update AuthenticationService.java

--- Backend/src/main/java/com/dalhousie/Neighbourly/authentication/service/AuthenticationService.java
+++ Backend/src/main/java/com/dalhousie/Neighbourly/authentication/service/AuthenticationService.java
@@ -2,18 +2,10 @@
 package com.dalhousie.Neighbourly.authentication.service;
 import com.dalhousie.Neighbourly.authentication.requestEntity.RegisterRequest;
 import com.dalhousie.Neighbourly.authentication.responseEntity.AuthenticationResponse;
-
 import jakarta.servlet.http.HttpServletRequest;
-
 import com.dalhousie.Neighbourly.authentication.requestEntity.AuthenticateRequest;
 import com.dalhousie.Neighbourly.authentication.requestEntity.OtpVerificationRequest;
 public interface AuthenticationService {
-
-    AuthenticationResponse registerUser(RegisterRequest registerRequest);
-    AuthenticationResponse authenticateUser(AuthenticateRequest authenticateRequest);
-    AuthenticationResponse verifyOtp(OtpVerificationRequest otpVarificationRequest);
-} 
-
     AuthenticationResponse registerUser(RegisterRequest registerRequest);  
     void resendOtp(String email);
     public String getURL(HttpServletRequest request);
@@ -21,5 +13,4 @@ public interface AuthenticationService {
     AuthenticationResponse verifyOtp(OtpVerificationRequest otpVerificationRequest);
     void forgotPassword(String email, String resetUrl);
     void resetPassword(String email, String password, String token);
-} 
-
+}
