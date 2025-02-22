f683de96e9fd733ff50a2a2e6b1970690a894b6b
Prosper Collins <pr782734@dal.ca>
2025-02-22 12:20:52 -0400

    Update ApplicationConfig.java

--- Backend/src/main/java/com/dalhousie/Neighbourly/config/ApplicationConfig.java
+++ Backend/src/main/java/com/dalhousie/Neighbourly/config/ApplicationConfig.java
@@ -38,8 +38,8 @@ public class ApplicationConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("prospercollins19@gmail.com");
-        mailSender.setPassword("srgjyedcawydiulk");
+        mailSender.setUsername("devpatel43543@gmail.com");
+        mailSender.setPassword("dfjragryfjtqnkwp");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
