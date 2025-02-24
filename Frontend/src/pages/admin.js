7de2f15979fe044cb6629e43c6001a66710694c5
Krishna <krishnatejnk@gmail.com>
2025-02-24 04:50:41 -0400

    feat17. fixed fetch and  approve create community requests

--- Frontend/src/pages/admin.js
+++ Frontend/src/pages/admin.js
@@ -21,7 +21,7 @@ const AdminPage = () => {
 
     const fetchNotifications = async (neighbourhoodId) => {
         try {
-            const response = await axios.get(`http://localhost:8081/api/help-requests/openCommunityRequests`);
+            const response = await axios.get(`http://172.17.2.103:8080/api/help-requests/openCommunityRequests`);
             console.log(response.data.data);
             setNotifications(response.data.data);
         } catch (error) {
