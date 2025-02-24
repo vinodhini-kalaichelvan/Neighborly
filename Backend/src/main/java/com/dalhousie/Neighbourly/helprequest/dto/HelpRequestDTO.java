da93da755c77057a14069c83cdeac0dd75d7bc2d
Krishna <krishnatejnk@gmail.com>
2025-02-24 02:58:31 -0400

    feat12. fixed conflicts

--- Backend/src/main/java/com/dalhousie/Neighbourly/helprequest/dto/HelpRequestDTO.java
+++ Backend/src/main/java/com/dalhousie/Neighbourly/helprequest/dto/HelpRequestDTO.java
@@ -1,13 +1,30 @@
 package com.dalhousie.Neighbourly.helprequest.dto;
 
+import com.dalhousie.Neighbourly.helprequest.model.HelpRequest;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class HelpRequestDTO {
     private int userId;
     private int neighbourhoodId;
     private String requestType;
+    private HelpRequest.RequestStatus status;
     private String description;
+
+    public HelpRequestDTO buiHelpRequestDTO(HelpRequest helpRequest) {
+        HelpRequestDTO dto = new HelpRequestDTO();
+        dto.setUserId(helpRequest.getUser().getId());
+        dto.setDescription(helpRequest.getDescription());
+        dto.setStatus(helpRequest.getStatus());
+        return dto;
+    }
 }
 
 
